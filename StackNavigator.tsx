88281bb45849fe9c5849423975aa7ee338a847ab
Corbin Poteet <corbin.poteet@gmail.com>
2023-01-26 14:21:20 -0600

    Add basic navigation example

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const StackNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>I am the Home Screen</Text>
+      <Button 
+        title="Login" 
+        onPress={() => navigation.navigate('Chat')}
+      />
     </View>
   )
 }
