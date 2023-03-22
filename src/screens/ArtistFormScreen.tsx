bf1f4f8387c9e2f362206cc8a294ed1fd0c0ae14
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-03-22 11:52:49 -0500

    Fixed appendpromotion

--- src/screens/ArtistFormScreen.tsx
+++ src/screens/ArtistFormScreen.tsx
@@ -6,25 +6,12 @@ import { useState } from 'react';
 import {push, ref, set, child, update} from 'firebase/database';
 import database from "../../firebaseConfig.tsx"; //ignore this error the interpreter is being stupid it works fine
 
-const addPromotion = (artist:string, track:string) => { //This function is unused, but writes data to DB with a simple overwrite, deleting all data in DB and inserting artist and track
-  set(ref(database, "ArtistPromos/SampleArtist/"),{
+const appendPromotion = (artist:string, track:string) => { //function to append data to DB
+  push(ref(database, "ArtistPromos/"+artist+"/"),{
     artistName: artist,
     trackName: track
-  });
-}
-
-const appendPromotion = (artist:string, track:string) => { //function to append data to DB
-  const updates = { //New JSON object to send to DB
-    newArtistSample:{
-      artistName : artist,
-      trackName : track
-    }
-  };
-  const newArtistKey = push(child(ref(database), 'ArtistPromos/SampleArtist/')).key; //Generate new key for posting location in DB
-
+  })
   Alert.alert("Successfully submitted data to DB");
-
-  return update(ref(database), updates) //push updates object to DB
 }
 
 const ArtistFormScreen = () => {
