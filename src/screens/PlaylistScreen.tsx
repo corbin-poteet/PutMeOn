2dee462d6ed9062c33563d93038d30044259b2f0
Nicholas Riley <93410434+Nicholas-Riley@users.noreply.github.com>
2023-03-27 09:42:57 -0500

    Playlist Screen renders spotify data

--- src/screens/PlaylistScreen.tsx
+++ src/screens/PlaylistScreen.tsx
@@ -35,26 +35,35 @@ const PlaylistScreen = () => {
         playlists.forEach(element => { 
           console.log(element.name); //Testing log for debugging
         });
-        console.log("Finished Then");
         setPlaylists(playlists);
     
         for(var i = 0; i < playlists.length; i++) {
-          let button = <View>
-          <TouchableOpacity>
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
-              <Image source={require('@assets/blank_playlist.png')} style={{ marginRight: 12, marginLeft: 10, width: 75, height: 75 }}/>
-              <Text style={{ fontWeight: 'bold', fontSize: 36, color: 'white'}}> HELLO {i}</Text>
-            </View>
-          </TouchableOpacity>
-          </View>
-          //console.log(button);
-          
-          result.push(button);
-          console.log(button)
-          console.log(i+" : "+result[i])
+          result.push(
+            {
+              "name": playlists[i].name,
+              "image": playlists[i].images[0] 
+            }
+          );
+    
+          console.log(result[i].name);
       }
-      setComponentHandler(result.map(type => <View/>));
+      const listItems = result.map(
+        (element) => {
+          return (
+            <View>  
+              <TouchableOpacity>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5, marginBottom: 5 }}>
+                  <Image source={ {uri: element.image.url}} style={{ marginRight: 12, marginLeft: 0, width: 50, height: 50 }}/>
+                  <Text style={{ fontWeight: 'bold', fontSize: 24, color: 'white'}}> {element.name} </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            )
+          }
+        )
+      setComponentHandler(listItems);
       setLoaded(true)
+      //https://www.geeksforgeeks.org/how-to-render-an-array-of-objects-in-reactjs/
     });
   }
 
@@ -67,27 +76,19 @@ const PlaylistScreen = () => {
     <View className='flex-1 justify-center'>
       <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} className="flex-1 items-center justify-center">
         <View className='absolute top-1'>
-          <Text className='text-white text-2xl px-3'>Songs that you like in Put Me On will be added to a playlist of your choice in Spotify: </Text>
+          <Text className='text-white text-2xl px-3'>Songs that you like in Put Me On will be added to a Playlist of your choice: </Text>
         </View>
         <View style={{padding: 10, flex: 1}}>
           <ScrollView style={{flex: 1, marginTop: 100}}>
             <TouchableOpacity>
-              <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
-                <Image source={require('@assets/blank_playlist.png')} style={{ marginRight: 12, marginLeft: 10, width: 75, height: 75 }}/>
-                <Text style={{ fontWeight: 'bold', fontSize: 36, color: 'white'}}>New Playlist</Text>
+              <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5, marginBottom: 5 }}>
+                <Image source={require('@assets/blank_playlist.png')} style={{ marginRight: 12, marginLeft: 0, width: 50, height: 50 }}/>
+                <Text style={{ fontWeight: 'bold', fontSize: 24, color: 'white'}}>New Playlist</Text>
               </View>
             </TouchableOpacity>
             
             { componentHandler }
-            
-            <TouchableOpacity>
-              <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
-                <Image source={require('@assets/blank_playlist.png')} style={{ marginRight: 12, marginLeft: 10, width: 75, height: 75 }}/>
-                <Text style={{ fontWeight: 'bold', fontSize: 36, color: 'white'}}>Playlist</Text>
-              </View>
-            </TouchableOpacity>
-            
-
+ 
           </ScrollView>
         </View>
       </LinearGradient>
