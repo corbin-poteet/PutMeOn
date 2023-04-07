33861661f73a12a4b10d264230380608743a8f3d
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-04-06 21:07:26 -0500

    Removed header from end, round, and score screens

--- src/screens/gamescreens/RoundScreen.tsx
+++ src/screens/gamescreens/RoundScreen.tsx
@@ -3,17 +3,9 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/core';
 import { LinearGradient } from 'expo-linear-gradient';
 
-
 const RoundScreen = () => { //Display App information
     
     const navigation = useNavigation();
-
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-          headerShown: true,
-          headerTitle: 'App Information',         
-        });
-      }, [navigation]);
 
     return (
       <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
@@ -24,5 +16,4 @@ const RoundScreen = () => { //Display App information
     );
 }
   
-
 export default RoundScreen
