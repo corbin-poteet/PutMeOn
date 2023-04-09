f5166f1aebf64a6e950308039cfe2b08fe52931b
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-04-08 19:06:46 -0500

    Added context to end screen

--- src/screens/gamescreens/EndScreen.tsx
+++ src/screens/gamescreens/EndScreen.tsx
@@ -2,18 +2,31 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/core';
 import { LinearGradient } from 'expo-linear-gradient';
+import useGameContext from '@/common/hooks/gameContext';
 
 const EndScreen = () => {
     
     const navigation = useNavigation();
+    const { round } = useGameContext();
 
-    return (
-      <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
-        <View style={{ padding: 10 }}>
-          <Text className='font-white absolute top-10'>This is the End Screen.</Text>
-        </View>
-      </LinearGradient>
-    );
+    if(round > 5) {
+      return (
+        <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
+          <View style={{ padding: 10 }}>
+            <Text className='font-white absolute top-10'>This is the End Screen.</Text>
+          </View>
+        </LinearGradient>
+        );
+    } else {
+      return (
+        <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
+          <View style={{ padding: 10 }}>
+            <Text className='font-white absolute top-10'>This is the End Screen.</Text>
+          </View>
+        </LinearGradient>
+        );
+    }
+    
 }
   
 export default EndScreen
