41b4e109333ba8dfa2969916269bc348af39008b
Atheer Auribi <atheer.auribi@gmail.com>
2023-04-09 01:24:49 -0500

    TakeMeOff

--- src/common/components/elements/Swiper.tsx
+++ src/common/components/elements/Swiper.tsx
@@ -406,7 +406,7 @@ const Swiper = (props: Props) => {
 
   
 
-
+// ***************************************** CARD RENDERING ********************************
   return (
 
     <CardsSwipe cards={tracks} renderCard={(track: SpotifyApi.TrackObjectFull) => {
