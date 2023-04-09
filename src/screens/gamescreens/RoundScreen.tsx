5dd909bf330f619f7dfbfc1c95698b6ec009ef5b
Nicholas Riley <93410434+Nicholas-Riley@users.noreply.github.com>
2023-04-08 21:05:24 -0500

    asdf

--- src/screens/gamescreens/RoundScreen.tsx
+++ src/screens/gamescreens/RoundScreen.tsx
@@ -23,7 +23,7 @@ const RoundScreen = () => {
     return (
       <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
         <View className='flex-1 justify-center'>
-          <Text className='text-white text-6xl text-center px-1 font-bold'>Round {round} lmao</Text>
+          <Text className='text-white text-6xl text-center px-1 font-bold'>Round {round}</Text>
         </View>
       </LinearGradient>
     );
