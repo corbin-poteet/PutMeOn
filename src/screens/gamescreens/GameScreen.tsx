9ca9a53d1beae1261663b63a3680abafe83cd0d6
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-04-09 04:41:57 -0500

    Progress on gamescreen,  ending in score screen

--- src/screens/gamescreens/GameScreen.tsx
+++ src/screens/gamescreens/GameScreen.tsx
@@ -9,16 +9,36 @@ import { Audio } from 'expo-av';
 import database from "../../../../../firebaseConfig.tsx"; //ignore this error the interpreter is being stupid it works fine
 import { push, ref, set, child, get } from 'firebase/database';
 
+var questionTypes: string[] = ['artist name', 'album name', 'name']; //lmao classic instance variable moment
+  
 const GameScreen = () => {
 
   const navigation = useNavigation();
   const { round, setRound } = useContext(gameContext);
+  const { spotify, user } = useAuth();
 
-  const [tracks, setTracks] = React.useState<SpotifyApi.TrackObjectFull[]>([]);
   const [sound, setSound] = React.useState<Audio.Sound>();
   const [isPlaying, setIsPlaying] = React.useState<boolean>(false);
   const [playbackPosition, setPlaybackPosition] = React.useState<number>(0);
   const [playbackDuration, setPlaybackDuration] = React.useState<number>(0);
+
+  const [tracks, setTracks] = React.useState<SpotifyApi.TrackObjectFull[]>([]);
+  const [correctTrack, setCorrectTrack] = React.useState<SpotifyApi.TrackObjectFull>();
+
+  var questionType  = questionTypes[Math.floor(Math.random() * questionTypes.length)]; //randomize question type for the round (instance varibale Xdddd)
+
+  console.log("EUHHHHGGHHHHHHHHHHH"); //its 4 in the morning please kill me
+
+  //Pseudocode for this part:
+  //DONE 1. Choose a guess type randomly (artist, album, or track)
+  //1. Pull 4 tracks from API and put them in an array
+  //2. Put the first track in the array into the correctTrack state
+  //3. Shuffle the array
+  //4. Put the shuffled array tracks into the four buttons
+  //5. Play the correct track
+  //6. When the user clicks a button, check if it's the correct track
+  //7. Inform the user if correct, increment score and earnings, and round, and navigate to round screen again
+  //8. Inform the user if incorrect and navigate to round screen again
 
   return (
     <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
@@ -61,26 +81,26 @@ const GameScreen = () => {
       {/*Question*/}
       
       <View className='flex-1 justify-center'>
-        <Text className='p-2 text-white text-3xl font-bold'>What is the _ of this _?{/*Question details here*/}</Text>
+        <Text className='p-2 text-white text-3xl font-bold'>What is the {questionType} of this track?{/* Add variable instead of _ later with question type */}</Text>
         
         <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
           onPress={() => { navigation.navigate('Score') }}>
-          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks{}</Text>
+          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Next screen (testing only)</Text>
         </TouchableOpacity>
 
         <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
-          onPress={() => { setRound(70); console.log("AHHHHHHH"); }}>
-          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks{}</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
-          onPress={() => { /*Navigate based on correct, etc.*/ }}>
-          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks{}</Text>
+          onPress={() => { setRound(round + 1); }}>
+          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Increment round (testing only)</Text>
         </TouchableOpacity>
 
         <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
           onPress={() => { /*Navigate based on correct, etc.*/ }}>
-          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks{}</Text>
+          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
+          onPress={() => { /*Navigate based on correct, etc.*/ }}>
+          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks</Text>
         </TouchableOpacity>
       </View>
 
--- src/screens/gamescreens/ScoreScreen.tsx
+++ src/screens/gamescreens/ScoreScreen.tsx
@@ -1,13 +1,13 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigation } from '@react-navigation/core';
 import { LinearGradient } from 'expo-linear-gradient';
-import useGameContext from '@/common/hooks/gameContext';
+import gameContext from '@/common/hooks/gameContext';
 
 const ScoreScreen = () => {
     
     const navigation = useNavigation();
-    const { round, score, earnings } = useGameContext();
+    const { round, score, earnings } = useContext(gameContext);
 
     React.useLayoutEffect(() => {
       navigation.setOptions({
@@ -16,20 +16,38 @@ const ScoreScreen = () => {
       });
     }, [navigation]);
 
-    return (
-      <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
+    if(round >= 5) {
+      //DUETO: update database leaderboard with score here
+      return(
+        <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
         <View className='flex-1 justify-center'>
-          <Text className='text-white text-6xl text-center px-1 font-bold'>Correct!</Text>
-          <Text className='text-white text-4xl text-center p-5 font-bold'>Round {round}</Text>
-          <Text className='text-white text-2xl text-center px-2 font-bold'>You earned {earnings} points</Text>
-          <Text className='text-white text-2xl text-center px-2 font-bold'>Total Score: {score}</Text>
+          <Text className='text-white text-6xl text-center px-1 font-bold'>Game Over!</Text>
+          <Text className='text-white text-2xl text-center px-2 font-bold'>You earned {earnings} points that round!</Text>
+          <Text className='text-white text-2xl text-center px-2 font-bold'>Final Score: {score}</Text>
           <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
-            onPress={() => { navigation.navigate('Round') }}>
-          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks{}</Text>
+            onPress={() => { navigation.navigate('End') }}>
+          <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks</Text>
         </TouchableOpacity>
         </View>
-      </LinearGradient>
-    );
+        </LinearGradient>
+      );
+    }
+
+    else{
+      return (
+        <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
+          <View className='flex-1 justify-center'>
+            <Text className='text-white text-6xl text-center px-1 font-bold'>Get fucked lmao</Text>
+            <Text className='text-white text-2xl text-center px-2 font-bold'>You earned {earnings} points that round!</Text>
+            <Text className='text-white text-2xl text-center px-2 font-bold'>Total Score: {score}</Text>
+            <TouchableOpacity className="flex-row items-center justify-center bg-green-500 px-2 m-2 rounded-3xl"
+              onPress={() => { navigation.navigate('Round') }}>
+            <Text className="text-white text-xl px-8 py-2 text-1 font-semibold">Multiple Decks</Text>
+          </TouchableOpacity>
+          </View>
+        </LinearGradient>
+      );
+    }
 }
   
 export default ScoreScreen
