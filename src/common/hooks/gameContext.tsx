b140ed6509f292f50650aa86e58d7925b4688d6a
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-04-08 23:39:15 -0500

    added some stuff

--- src/common/hooks/gameContext.tsx
+++ src/common/hooks/gameContext.tsx
@@ -3,13 +3,16 @@ import React, { createContext, useContext, useState } from 'react'
 const gameContext = createContext({
     round: 0,
     score: 0,
-    earnings: 0
+    earnings: 0,
+    setRound: () => {},
+    setScore: () => {},
+    setEarnings: () => {}
 })
 
 export const GameProvider = ({children}) => {
 
     return (
-        <gameContext.Provider value={{ round: 69, score: 69, earnings: 69 }}>
+        <gameContext.Provider value={{ round: 69, score: 69, earnings: 69, }}>
             {children}
         </gameContext.Provider>
     )
--- src/screens/gamescreens/RoundScreen.tsx
+++ src/screens/gamescreens/RoundScreen.tsx
@@ -3,31 +3,21 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/core';
 import { LinearGradient } from 'expo-linear-gradient';
 import useGameContext from '@/common/hooks/gameContext';
+import { useContext } from 'react';
 
 const RoundScreen = () => {
     
     const navigation = useNavigation();
     const { round } = useGameContext();
-
-    React.useLayoutEffect(() => {
-      navigation.setOptions({
-        gestureEnabled: true, //can be set to false to disable swipe out of page
-        gestureDirection: 'horizontal',
-      });
-    }, [navigation]);
     
-    React.useEffect(() => {
-      const timeout = setTimeout( () => {
-        navigation.navigate('Game'); //du-
-      }, 2000 );
+    setTimeout( () => {
+      navigation.navigate('Game');
+    }, 2000 );
 
-      return () => clearTimeout(timeout);  
-    }, []);
-  
     return (
       <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
         <View className='flex-1 justify-center'>
-          <Text className='text-white text-6xl text-center px-1 font-bold'>Round {round}</Text>
+          <Text className='text-white text-6xl text-center px-1 font-bold'>Round {round} lmao</Text>
         </View>
       </LinearGradient>
     );
