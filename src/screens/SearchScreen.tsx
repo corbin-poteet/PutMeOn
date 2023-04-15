92f0632a01bf1269dbe48d358ed7908e1f87b763
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-04-14 21:37:45 -0500

    More search screen functionality

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -7,19 +7,23 @@ import useAuth from '@/common/hooks/useAuth';
 import { ref, child, get, set } from 'firebase/database';
 // @ts-ignore
 import database from "../../firebaseConfig.tsx";
+import { ScrollView } from 'react-native-gesture-handler';
 
 
-let searchResults: any[];
+let searchResults: any[] = [];
 
 const SearchScreen = () => {
 
     const { spotify, user } = useAuth();
     const navigation = useNavigation();
 
+    const result: any[] = []; //holds search results in getSearchResults function
+
     const [toggle, setToggle] = useState<boolean>(false); //false for genre search, true for artist search
     const [search, setSearch] = useState<string>(''); //keeps track of text entered in search bar dynamically
     const [loaded, setLoaded] = useState<boolean>(false); //keeps track of if a screen is done loading
-    const [componentHandler, setComponentHandler] = React.useState<any>(); //keeps track of search results
+    const [componentHandler, setComponentHandler] = useState<any>(); //keeps track of search results
+    const [seeds, setSeeds] = useState<any>([]); //holds up to 5 seeds to pass to next screen
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -37,32 +41,36 @@ const SearchScreen = () => {
 
     async function getSearchResults() {
 
-        const response = await spotify.getUserPlaylists(user?.id, { limit: 50 }
-        ).then(
-          function (data) {
-            searchResults = data.items;
-    
-            const listItems = result.map(
-              (element) => {
-                return (
-                  <View>
-                    <TouchableOpacity onPress={
-                      () => {
-                        //add
+        if(toggle){ //if toggle true: search for artists
+            const response = await spotify.searchArtists(search, { limit: 20 }
+                ).then(
+                  function (data) {
+                    searchResults = data.artists; //needs some kind of change here cannot figure this out rn
+                    
+                    const listItems = result.map(
+                      (element) => {
+                        return (
+                          <View>
+                            <TouchableOpacity onPress={
+                              () => {
+                                //add
+                              }
+                            }>
+                              <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5, marginBottom: 5 }}>
+                                <Text style={{ fontWeight: 'bold', fontSize: 24, color: 'white' }}> {element.name} </Text>
+                              </View>
+                            </TouchableOpacity>
+                          </View>
+                        )
                       }
-                    }>
-                      <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5, marginBottom: 5 }}>
-                        <Text style={{ fontWeight: 'bold', fontSize: 24, color: 'white' }}> {element.name} </Text>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                )
-              }
-            )
-            setComponentHandler(listItems);
-            setLoaded(true)
-            //https://www.geeksforgeeks.org/how-to-render-an-array-of-objects-in-reactjs/
-          });
+                    )
+                    setComponentHandler(listItems);
+                    setLoaded(true)
+                  });
+        }
+        else{ //if toggle false: search for genres
+
+        }
       }
 
     return (
@@ -75,6 +83,7 @@ const SearchScreen = () => {
                     <Text className="text-white text-xl px-5 py-2 text-1 font-semibold text-center">This is the search screen. It looks like shit right now but it will allow you to search for 5 seed artists/genres for making a new deck</Text>
                     <TextInput placeholderTextColor={"#0B0B45"} placeholder='Search' onChangeText={setSearch} className='mx-5 font-semibold text-1 text-white text-xl flex-row items-center justify-center rounded-3xl top-5 px-8 py-2.5' style={{ backgroundColor: '#014871' }}></TextInput>
                 </View>
+                <ScrollView>{componentHandler}</ScrollView>
             </LinearGradient>
         </View>
     )
