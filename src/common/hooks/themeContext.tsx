1d194588e7dc5f6b78fea31198ef1a13bb3bef79
Nicholas-Riley <njril421@gmail.com>
2023-04-26 16:03:26 -0500

    ghoul gabbin

---
+++ src/common/hooks/themeContext.tsx
@@ -0,0 +1,72 @@
+import { createContext } from 'react'
+
+const themeContext = createContext({
+   name1: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name2: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name3: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name4: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name5: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name6: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name7: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   name8: {
+    topGradient: "",
+    bottomGradient: "",
+    topCard: "",
+    bottomCard: "",
+    button: "",
+    misc: "",
+   },
+   
+   setTheme: {},
+});
+
+export default themeContext;
