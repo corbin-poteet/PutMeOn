1a2ccc3f6bb45503dc1df7605403ad2f919f24fa
Nicholas Riley <93410434+Nicholas-Riley@users.noreply.github.com>
2023-04-26 23:27:42 -0500

    theme button functionality

--- src/screens/Themes.tsx
+++ src/screens/Themes.tsx
@@ -3,9 +3,11 @@ import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native
 import { useNavigation } from '@react-navigation/core';
 import useAuth from '@hooks/useAuth';
 import { LinearGradient } from 'expo-linear-gradient';
+import useTheme from '@/common/hooks/useThemes';
 
 const Themes = () => {
   const navigation = useNavigation();
+  const { setSelectedTheme, selectedTheme } = useTheme(); //Allows dynamic theme color changing
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -17,7 +19,7 @@ const Themes = () => {
   //Theme list. Increment ID to add more.
   const themes = [
     { id: '1', label: 'Default', colors: ['#d9d9d9', '#4a4a4a'] },
-    { id: '2', label: 'PMO Blue', colors: ['#abc7f7', '#005eff'] },
+    { id: '2', label: 'PMOBlue', colors: ['#abc7f7', '#005eff'] },
     { id: '3', label: 'Carbon', colors: ['#333', '#111'] },
     { id: '4', label: 'Mango', colors: ['#ff8c00', '#4b941b'] },
     { id: '5', label: 'Lemon', colors: ['#f5cc00', '#2f6b2e'] },
@@ -25,13 +27,14 @@ const Themes = () => {
     { id: '7', label: 'Firestone', colors: ['#a80707', '#210502'] },
     { id: '8', label: 'Volcano', colors: ['#e35e40', '#3d0f04'] },
     { id: '9', label: 'Legacy', colors: ['#696969', '#696969'], labelColor: '#82f252' }
-
-
   ];
 
   //@ts-ignore
   const ThemeButton = ({ theme }) => (
-    <TouchableOpacity style={styles.button}>
+    <TouchableOpacity style={styles.button} onPress={() => {
+      //@ts-ignore
+      setSelectedTheme(theme.label);
+    }}> 
       <LinearGradient
         colors={theme.colors}
         style={styles.circle}
