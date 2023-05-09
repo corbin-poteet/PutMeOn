d357d130b559c43b9dc0ce4ca38d0cb3a01a321b
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-05-08 23:17:45 -0500

    Update InfoScreen.tsx

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -18,8 +18,8 @@ const AppInfo = () => { //Display App information
   return (
     <LinearGradient start={{ x: -0.5, y: 0 }} colors={['#014871', '#A0EBCF']} style={{ flex: 1, justifyContent: 'flex-start' }}>
       <View style={{ padding: 10 }}>
-        <Text style={{ fontSize: 20, color: 'white' }}>Contact PMO support at: Your@Mom.com</Text>
-        <Text style={{ fontSize: 20, color: 'white' }}>Put Me On v0.9.8 (The Put It Away update)</Text>
+        <Text style={{ fontSize: 20, color: 'white' }}>Contact PMO support at: support@putmeon.com</Text>
+        <Text style={{ fontSize: 20, color: 'white' }}>Put Me On v1.0 (The Put It Together update)</Text>
       </View>
     </LinearGradient>
   );
